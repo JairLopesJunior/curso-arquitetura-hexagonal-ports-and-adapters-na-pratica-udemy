fe4741604ccc0e409e5cc5388a8bfe14874438d1
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2024-10-21 19:07:28 -0300

    Criando o consumer

---
+++ 08 - Criacao producer consumer kafka/Aula23 - Criando o consumer/hexagonal/src/main/java/com/lopes/hexagonal/config/FindCustomerByIdConfig.java
@@ -0,0 +1,17 @@
+package com.lopes.hexagonal.config;
+
+import com.lopes.hexagonal.adapters.out.FindCustomerByIdAdapter;
+import com.lopes.hexagonal.application.core.usecase.FindCustomerByIdUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FindCustomerByIdConfig {
+
+    @Bean
+    public FindCustomerByIdUseCase findCustomerByIdUseCase(
+            FindCustomerByIdAdapter findCustomerByIdAdapter
+    ) {
+        return new FindCustomerByIdUseCase(findCustomerByIdAdapter);
+    }
+}
