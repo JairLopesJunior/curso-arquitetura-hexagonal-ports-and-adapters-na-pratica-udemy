90333892480d395621230f673590cdbdb8c26466
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2024-10-21 16:44:05 -0300

    Criando as configuraçoes do Kafka

---
+++ 07 - Criacao das configuracoes/Aula21-Criando configuracoes Kafka/hexagonal/src/main/java/com/lopes/hexagonal/application/ports/out/UpdateCustomerOutputPort.java
@@ -0,0 +1,8 @@
+package com.lopes.hexagonal.application.ports.out;
+
+import com.lopes.hexagonal.application.core.domain.Customer;
+
+public interface UpdateCustomerOutputPort {
+
+    void update(Customer customer);
+}
