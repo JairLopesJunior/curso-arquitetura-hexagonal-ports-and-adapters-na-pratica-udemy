04a95d4a60543bd9539513e2f604e27e0cf14797
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2024-10-22 09:59:33 -0300

    Rodando a aplicação

---
+++ 09 - Configuracao rodar aplicacacao/Aula25-Subindo o Kafka MongoDB/hexagonal/src/main/java/com/lopes/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -0,0 +1,53 @@
+package com.lopes.hexagonal.adapters.out;
+
+import com.lopes.hexagonal.adapters.out.repository.CustomerRepository;
+import com.lopes.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.lopes.hexagonal.application.core.domain.Customer;
+import com.lopes.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void insert(Customer customer) {
+        var customerEntity = this.customerEntityMapper.toCustomerEntity(customer);
+        this.customerRepository.save(customerEntity);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
